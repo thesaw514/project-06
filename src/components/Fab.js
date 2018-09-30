20181082f950d1ff444f177d43fd138b86ca3995
thesaw514 <thesaw514@gmail.com>
2018-09-30 16:55:47 -0400

    Wiring Up The (FAB) Floating Action Button

--- src/components/Fab.js
+++ src/components/Fab.js
@@ -1,11 +1,13 @@
 // Theodore Sawyer, FEND - Project 06: 'MyReads: A Book Tracking App' / Fab.js / 09.29.18
 
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Fab extends Component {
   render() {
-    return(<div className="open-search">
-      <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+    return(
+      <div className="open-search">
+        <Link to={'/search'}>Add a book</Link>
     </div>
     );
   }
